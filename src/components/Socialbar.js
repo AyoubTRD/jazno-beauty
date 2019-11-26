c949eddf9525a7d17b44246980d2843cb5cf4e65
JrTRD <ayoubtrdbuzzer@gmail.com>
2019-11-26 19:41:05 +0100

    added whatsapp link + _redirects

--- src/components/Socialbar.js
+++ src/components/Socialbar.js
@@ -3,13 +3,19 @@ import React from "react";
 import { FaWhatsapp as Whatsapp } from "react-icons/fa";
 
 const Socialbar = () => {
+  const phone = "212607777246";
+  const text = "أنا مهتم";
   return (
-    <div className="bar">
+    <a
+      className="bar"
+      target="blank"
+      href={`https://wa.me/${phone}?text=${encodeURI(text)}`}
+    >
       <div className="bar-icon bar-icon-whatsapp">
         <Whatsapp className="bar-icon-icon" />
         <p className="bar-icon-text">راسلنا عبر الواتساب</p>
       </div>
-    </div>
+    </a>
   );
 };
 
